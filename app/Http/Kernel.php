b803fc9009d460029d31da69df1230679390bd91
everyback <543245543@qq.com>
2019-04-12 22:07:24 +0800

    暂时中止

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -45,7 +45,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            'throttle:60,1',
+            'throttle:240,1',
             'bindings',
             'ReturnJSON' => \App\Http\Middleware\ReturnJSON::class,
             'FreshToken'=> \App\Http\Middleware\FreshToken::class,
